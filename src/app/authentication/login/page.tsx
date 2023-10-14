14e5b7c89b8eeceb1beb0cf52b195b95bf0c71e5
saidelhabchi <elhabchisaid40@gmail.com>
2023-10-14 15:37:08 +0100

    authentication page first design

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import Link from 'next/link'
 const Login = () => {
     return ( 
-        <div className="h-full p-16">
+        <div className="h-full py-5 px-10">
             <div>
                 <TextField
                 fullWidth={true}
@@ -11,7 +12,7 @@ const Login = () => {
                 label="email"
                 value={""}
                 variant='standard'
-                onChange={()=>{console.log("change")}}  
+                onChange={()=>{console.log("change")}} 
                 />
             </div>
             <div>
@@ -24,11 +25,18 @@ const Login = () => {
                 onChange={()=>{console.log("change")}}  
                 />
             </div>
-            <div>
-                <Button variant="text" color="success">
+            <div className='flex justify-center items-center pt-5'>
+                <p>don't have account ? <span className='text-emerald-600 cursor-pointer'><Link href={'/authentication/register'}>
+                        Register
+                    </Link></span>
+                </p>
+            </div>
+            <div className='flex justify-center items-end pt-5'>
+                <Button variant="text" color="success" className='bg-emerald-800 text-green-100 hover:bg-emerald-600 capitalize'>
                 Login
                 </Button>
             </div>
+            
         </div>
      );
 }
