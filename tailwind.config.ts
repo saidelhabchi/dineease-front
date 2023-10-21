39312e4da89fea641769709849ac14ec731a8c53
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-10-21 12:57:10 +0100

    Require Next-Auth.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        '3xl': '0 3px 10px rgb(0 0 0 / 0.2)',
+      }
     },
   },
   plugins: [],
